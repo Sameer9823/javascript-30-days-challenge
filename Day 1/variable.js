4266a4d7329b17811183db74bac52d93dd5a3510
Sameer9823 <sameerselokar924@gmail.com>
2024-07-13 19:45:43 +0530

    Variables

---
+++ Day 1/variable.js
@@ -0,0 +1,70 @@
+var num = 23;
+console.log(num);
+
+let string = "sameer"
+console.log(string);
+
+const boolean = false
+console.log(boolean);
+
+let number = 334
+console.log(typeof(number));
+
+let character = "sameer"
+console.log(typeof(character));
+
+let array = ['as','df','rt','df']
+console.log(Array.isArray(array));
+
+let sam = {
+    name: "sameer",
+    age: 23,
+}
+
+console.log(typeof(sam));
+
+let a = "sameer";
+console.log(a);
+
+a = 233;
+console.log(a);
+
+const b = 234;
+// console.log(b);
+
+// b = 7878;
+// console.log(b);
+
+
+let c = 'sameer';
+const d = 123;
+let arr = [1,2,3,4,5,6];
+let obj = {name: 'geet', age:21};
+function sam1(){
+    return 'sameer'
+}
+// console.log(sam());
+
+let nullvar = null;
+
+console.log(`${c}: type = ${typeof(c)}`);
+console.log(`${d}: type = ${typeof(d)}`);
+console.log(`${arr}: type = ${Array.isArray(arr)}`);
+console.log(`${obj}: type = ${typeof(obj)}`);
+console.log(`${sam1()}: type = ${typeof(sam)}`);
+console.log(`${nullvar}: type = ${typeof(nullvar)}`);
+
+
+
+console.log("Demonstrate let variable:");
+
+let initialvalue = "initialvalue";
+console.log(initialvalue);
+
+initialvalue = "update value";
+console.log(initialvalue);
+
+const initialvalue1 = "1 value"
+console.log(initialvalue1);
+
+// initialvalue1 = "update" typeerror: assignment to constant variable
