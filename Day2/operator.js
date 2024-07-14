320cca4533c9280ef01f60d93bf39452de3d42ee
Sameer9823 <sameerselokar924@gmail.com>
2024-07-14 13:15:55 +0530

    Operators in JS

---
+++ Day2/operator.js
@@ -0,0 +1,108 @@
+//Addition
+let  a = 23;
+let b = 45;
+let c = a + b;
+console.log(c);
+
+//substraction
+let d = 122;
+let e = 45;
+console.log(d - e);
+
+//multiplication
+let f = 45;
+let g = 45;
+console.log(f * g);
+
+//divide 
+let h = 45;
+let i = 45;
+console.log(h / i);
+
+//remainder
+let j = 45;
+let k = 7;
+console.log(j % k);
+
+//assignment operator
+let l = 45;
+let m = 30;
+l = l += m;
+console.log(l);
+
+let v = 56;
+let w = 45;
+v = v -= w;
+console.log(v);
+
+//comparison operator
+let x = 45;
+let y = 50;
+console.log(x < y);
+
+let n = 90;
+let o = 99;
+console.log(n > o);
+
+let ss = 97;
+let tt = 100;
+console.log(ss >= tt);
+
+let nn = 23;
+let oo = 222;
+console.log(nn <= oo);
+
+let kk = 55;
+let ll = '55';
+console.log(kk == ll);
+console.log(kk === ll);
+
+//logical operator
+
+let jj = 44;
+let ke = 44;
+console.log(jj == ke && ke == ll);
+console.log(jj == ke || ke == ll);
+
+let yy = true;
+let zz = false;
+console.log(!yy);
+console.log(!zz);
+
+
+//ternary operator
+let i1 = 5;
+i1 === 5 ? console.log('positive') : console.log('negative');
+
+//Arithematic operation script
+console.log(`Addtion of 23 + 45: result is ${c}`);
+console.log(`subtraction of 144 - 45: result is ${d - e}`);
+console.log(`multiplication of 45 * 45: result is ${f * g}`);
+console.log(`Divide of 45 / 45: result is ${h / i}`);
+console.log(`modulus of 45 % 7: result is ${j % k}`);
+
+//comparison operators
+console.log(`x is less than y: ${x < y}`);
+console.log(`x is greater than y: ${x > y}`);
+console.log(`ss is greater than or equal to tt: ${ss >= tt}`);
+console.log(`nn is less than or equal to oo: ${nn <= oo}`);
+
+//logical operator
+console.log(`jj is equal to ke and ke is equal to ll: ${jj == ke && ke == ll}`);
+console.log(`jj is equal to ke or ke is equal to ll: ${jj == ke || ke == ll}`);
+console.log(`not true: ${!yy}`);
+console.log(`not false: ${!zz}`);
+
+//ternary operator
+console.log(`i1 == 5 ? positive or negative = ${i1 === 5 ? 'positive' : 'negative'}`);
+
+
+
+
+
+
+
+
+
+
+
